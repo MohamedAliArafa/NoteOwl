8ff25393a8f4ee172694324438707b506b013f93
SneakySenpai <amgadmedhat@outlook.com>
2016-08-28 13:24:39 +0200

    New Task Page layout unFinished yet

--- app/src/main/java/com/zeowls/noteowl/MainActivity.java
+++ app/src/main/java/com/zeowls/noteowl/MainActivity.java
@@ -1,15 +1,14 @@
 package com.zeowls.noteowl;
 
-import android.database.Cursor;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.zeowls.noteowl.provider.Contract;
 import com.zeowls.noteowl.provider.DBHelper;
@@ -25,12 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+                                   @Override
+                                   public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                                       /**
+                                        *Intent for the new task
+                                        */
+                                       //TODO
+                                       Intent intent = new Intent(MainActivity.this, NewTaskActivity.class);
+                                       startActivity(intent);
+                                   }
+                               }
+
+        );
     }
 
     @Override
